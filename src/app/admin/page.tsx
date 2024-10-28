ba87bb9d3f04e73fe70b55b1179da7efd5124fdb
anwaar001 <anwaar6260@gmail.com>
2024-10-28 20:58:51 +0500

    changed mainnet

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -31,7 +31,7 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const { autoConnect } = useAutoConnect();
 
   // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => web3.clusterApiUrl(network), [network]);
