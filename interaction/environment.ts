bd80d9dd1ec9a5ded4f09b0f6d7aacb747dd4b75
anwaar001 <anwaar6260@gmail.com>
2024-11-01 18:41:55 +0500

    changed rpc

--- interaction/environment.ts
+++ interaction/environment.ts
@@ -1,2 +1,2 @@
 import { Connection, clusterApiUrl } from "@solana/web3.js";
-export const connection = new Connection("https://divine-nameless-snow.solana-mainnet.quiknode.pro/cbf3ffcb19633575cc0d278031b1c28c7e538633", 'confirmed');
+export const connection = new Connection("https://rpc.shyft.to/?api_key=6qgEDmJbmOqBsbXq", 'confirmed');
